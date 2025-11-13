e23fda6d7a1758a2424e6e6fce35dc53d57e29f0
Codearlo <flavio.cabanah@gmail.com>
2025-11-13 09:41:30 -0500

    estructurado

--- public/store/header/store-header.js
+++ public/store/header/store-header.js
@@ -130,7 +130,7 @@ document.addEventListener('DOMContentLoaded', () => {
             
             if (userRole === 'admin') {
                 authenticatedLinks += `
-                    <a href="/admin/dashboard.html" class="dropdown-item">
+                    <a href="admin/dashboard/dashboard.html" class="dropdown-item">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="3" x2="9" y2="21"></line><line x1="15" y1="3" x2="15" y2="21"></line><line x1="3" y1="9" x2="21" y2="9"></line><line x1="3" y1="15" x2="21" y2="15"></line></svg>
                         <span>Dashboard Admin</span>
                     </a>
@@ -179,4 +179,4 @@ document.addEventListener('DOMContentLoaded', () => {
     if (headerContainer) {
         loadComponent('public/store/header/store-header.html', headerContainer);
     }
-});   
+});
